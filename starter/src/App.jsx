cefea2556ab8e5b521dab33a8fe5c44e4bb78e09
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-04-18 23:10:16 +0200

    nav

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -12,11 +12,37 @@ const router = createBrowserRouter([
   {
     path:'/',
     element: <HomeLayout/>,
+    children:[
+      {
+        index: true,
+        element: <Landing/>,
+      },
+      {
+        path:'cocktail',
+        element: <Cocktail/>,
+      },
+      {
+        path:'newsletter',
+        element: <Newsletter/>,
+      },
+      {
+        path:'about',
+        element: <About/>,
+        children: [
+          {
+            index: true,
+            element: <h2>our company</h2>,
+          },
+          {
+            path:'person',
+            element: <h2>Karolina Starowicz</h2>,
+          },
+        ]
+      },
+
+    ]
   },
-  {
-    path:'/about',
-    element: <About/>,
-  },
+ 
 ]);
 
 const App = () => {
--- starter/src/pages/About.jsx
+++ starter/src/pages/About.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 
 const About = () => {
     return (
     <div>
         <h1>About</h1>
-        <Link to='/'>Home Page</Link>
+        <Outlet/>
      </div>
 
 );
