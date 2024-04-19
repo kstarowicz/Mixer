4fea7dd46db979c9b340d4ecc9f0a99415d6c926
kastarowicz@gmail.com <106586960+kstarowicz@users.noreply.github.com>
2024-04-20 00:38:09 +0200

    coctailCard

--- starter/src/components/CoctailCard.jsx
+++ starter/src/components/CoctailCard.jsx
@@ -1,4 +1,22 @@
-const CoctailCard = () => {
-    return  <h2>CoctailCard</h2>;    
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/CocktailCard";
+
+
+const CoctailCard = ({image, name, id, info, glass}) => {
+    return  (
+        <Wrapper>
+            <div className="img-container">
+                <img src={image} alt={name} className="img"/>
+            </div>
+            <div className="footer">
+                <h4>{name}</h4>
+                <h5>{glass}</h5>
+                <p>{info}</p>
+                <Link to={`/cocktail/${id}`} className="btn">
+                    details 
+                </Link>
+            </div>
+        </Wrapper>
+    )    
 };
 export default CoctailCard;
